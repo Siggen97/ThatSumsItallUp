76d9cfc0ea0b97c8129471de6b5f8c8fd392bf1e
Siggen97 <104718385+Siggen97@users.noreply.github.com>
2023-09-27 18:52:49 +0200

    Update

--- js/archivescript.js
+++ js/archivescript.js
@@ -24,23 +24,36 @@ function fetchPosts(perPage) {
 
 	fetch(apiUrl)
 		.then((response) => response.json())
-		.then((posts) => displayPosts(posts))
+		.then((posts) => posts.forEach(fetchAndDisplayPost))
 		.catch((error) => console.error('Error fetching posts:', error));
 }
 
-function displayPosts(posts) {
-	const postsContainer = document.querySelector('.all-posts');
-
-	posts.forEach((post) => {
-		const postElement = document.createElement('div');
-		postElement.className = 'post-summary';
-		postElement.innerHTML = `
-            <h3>${post.title.rendered}</h3>
-            <p>${post.excerpt.rendered}</p>
-            <p>Author: ${authors[post.author] || 'Unknown'}</p>
-            <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
-            <a href="post.html?id=${post.id}">Read more</a>
-        `;
-		postsContainer.appendChild(postElement);
-	});
+function fetchAndDisplayPost(post) {
+	fetch(
+		`https://www.thatsumsitallup.site/wp-json/wp/v2/media/${post.featured_media}`
+	)
+		.then((response) => response.json())
+		.then((media) => {
+			post.mediaUrl = media.source_url;
+			displayPost(post);
+		})
+		.catch((error) => console.error('Error fetching media:', error));
 }
+
+function displayPost(post) {
+	const postsContainer = document.querySelector('.all-posts');
+	const postElement = document.createElement('div');
+	postElement.className = 'post-summary';
+	postElement.innerHTML = `
+        <a href="post.html?id=${post.id}">
+            <h3>${post.title.rendered}</h3>
+            <img src="${post.mediaUrl}" alt="${post.title.rendered}">
+        </a>
+        <p>${post.excerpt.rendered}</p>
+        <p>Author: ${authors[post.author] || 'Unknown'}</p>
+        <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
+        <a href="post.html?id=${post.id}">Read more</a>
+    `;
+	postsContainer.appendChild(postElement);
+}
+
--- js/homescript.js
+++ js/homescript.js
@@ -13,23 +13,35 @@ function fetchPosts(perPage) {
 
 	fetch(apiUrl)
 		.then((response) => response.json())
-		.then((posts) => displayPosts(posts))
+		.then((posts) => posts.forEach(fetchAndDisplayPost))
 		.catch((error) => console.error('Error fetching posts:', error));
 }
 
-function displayPosts(posts) {
-	const postsContainer = document.querySelector('.latest-posts');
+function fetchAndDisplayPost(post) {
+	fetch(
+		`https://www.thatsumsitallup.site/wp-json/wp/v2/media/${post.featured_media}`
+	)
+		.then((response) => response.json())
+		.then((media) => {
+			post.mediaUrl = media.source_url;
+			displayPost(post);
+		})
+		.catch((error) => console.error('Error fetching media:', error));
+}
 
-	posts.forEach((post) => {
-		const postElement = document.createElement('div');
-		postElement.className = 'post-summary';
-		postElement.innerHTML = `
+function displayPost(post) {
+	const postsContainer = document.querySelector('.latest-posts');
+	const postElement = document.createElement('div');
+	postElement.className = 'post-summary';
+	postElement.innerHTML = `
+        <a href="post.html?id=${post.id}">
             <h3>${post.title.rendered}</h3>
-            <p>${post.excerpt.rendered}</p>
-            <p>Author: ${authors[post.author] || 'Unknown'}</p>
-            <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
-            <a href="post.html?id=${post.id}">Read more</a>
-        `;
-		postsContainer.appendChild(postElement);
-	});
+            <img src="${post.mediaUrl}" alt="${post.title.rendered}">
+        </a>
+        <p>${post.excerpt.rendered}</p>
+        <p>Author: ${authors[post.author] || 'Unknown'}</p>
+        <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
+        <a href="post.html?id=${post.id}">Read more</a>
+    `;
+	postsContainer.appendChild(postElement);
 }
